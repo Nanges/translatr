8aba9bda92c402f9a971477b3101a6f5d26ea5f8
unknown <3200283+Nanges@users.noreply.github.com>
2019-02-26 21:52:14 +0100

    Fix merge

--- translatr-client/src/app/app.module.ts
+++ translatr-client/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
